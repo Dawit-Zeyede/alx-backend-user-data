2829d5ea91e0e8234ed130bde5010d9ffe4dd434
Dawit-Zeyede <devazeyede10@gmail.com>
2024-11-14 14:47:25 +0000

    auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -3,14 +3,16 @@
 """
 import os
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+
+from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_db_auth import SessionDBAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -38,30 +40,28 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    '''
-    Unauthorized Users handler.
-    '''
+    """Unauthorized handler.
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    '''
-    Forbidden Users handler.
-    '''
+    """Forbidden handler.
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def authenticate_user():
-    '''
-    Authenticates a user before processing a request.
-    '''
+    """Authenticates a user before processing a request.
+    """
     if auth:
         excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/",
         ]
         if auth.require_auth(request.path, excluded_paths):
             user = auth.current_user(request)
